306fbb11bbd041e5e9634ff2059198ac487421cc
Nzalan <168459860+Zalan1885@users.noreply.github.com>
2025-01-31 16:27:31 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,12 @@
 // firebaseConfig.js
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID"
+    apiKey: "AIzaSyBEbcAjgwygXP6kpWmwJRd2OQSVI8Wet3A",
+    authDomain: "githubfirebase-57727.firebaseapp.com",
+    projectId: "githubfirebase-57727",
+    storageBucket: "githubfirebase-57727.firebasestorage.app",
+    messagingSenderId: "539084537408",
+    appId: "1:539084537408:web:dd616b97c4523f7530a9a5",
+    measurementId: "G-N24N6NHEG4"
 };
 
 // Initialize Firebase
